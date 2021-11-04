46888b110901f048db81907c26dd318d9f1b7edb
Abigail <kristalys.cba4@yahoo.com>
2021-11-04 12:29:10 -0400

    Logic tree nodes, selection at batch level works and there still a lot to do.

--- testORC/src/main/java/orc/ORC.java
+++ testORC/src/main/java/orc/ORC.java
@@ -1,5 +1,8 @@
 package orc;
 
+import org.apache.hadoop.hive.ql.io.sarg.PredicateLeaf;
+import org.apache.hadoop.hive.ql.io.sarg.SearchArgument;
+import org.apache.hadoop.hive.ql.io.sarg.SearchArgumentFactory;
 import org.apache.orc.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -10,45 +13,123 @@ import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Consumer;
 
+import org.apache.orc.filter.BatchFilter;
+import org.apache.orc.impl.RecordReaderImpl;
+import org.apache.orc.impl.filter.FilterFactory;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
+
+
+
 public class ORC {
+    static final int BATCH_SIZE = 100;
+
+
+
+    public static void readerPrint(String path)throws IOException {
+        Configuration conf = new Configuration();
+        Reader reader = OrcFile.createReader(new Path(path), OrcFile.readerOptions(conf));
+
+        RecordReader records = reader.rows();
+        VectorizedRowBatch batch = reader.getSchema().createRowBatch();
+
+        while (records.nextBatch(batch)) {
+            System.out.println(batch.toString());
+        }
+        records.close();
+    }
 
     public static void reader(String path, String projections) throws IOException {
         Configuration conf = new Configuration();
+//        OrcConf.READER_USE_SELECTED.setBoolean(conf, true);
+        OrcConf.ALLOW_SARG_TO_FILTER.setBoolean(conf, true);
         Reader reader = OrcFile.createReader(new Path(path), OrcFile.readerOptions(conf));
 
         TypeDescription schema = reader.getSchema();
         ArrayList<String> names = new  ArrayList<String>(schema.getFieldNames());
         ArrayList<String> project = new ArrayList<String>(Arrays.asList(projections.split(",")));
+
         boolean[] finalProjection = new boolean[names.size()+1];
         finalProjection[0] = true;
-
-        int contCol = 0;
+        int countCol = 0;
         for(int i = 0; i< names.size(); i++) {
             if(project.contains(names.get(i))){
                 finalProjection[i+1] = true;
-                contCol++;
+                countCol++;
             } else{
                 finalProjection[i+1] = false;
             }
         }
 
-        Reader.Options read = new Reader.Options();
-        read.include(finalProjection);
-        // read.setRowFilter() <-- can do the projection requires a little bit of study
-        RecordReader records = reader.rows(read);
+        Consumer<OrcFilterContext> consumer = gr ->
+        {
+            LongColumnVector cv = (LongColumnVector) gr.findColumnVector("val")[0];
+            int index = 0;
+            int[] selected = gr.getSelected();
+            for (int i = 0; i < cv.vector.length; i++) {
+                if(cv.vector[i] < 9){ // Condition
+                    selected[index] = i;
+                    index++;
+                }
+            }
+            gr.setSelectedInUse(true);
+            gr.setSelected(selected);
+            gr.setSelectedSize(index);
+        };
 
-        VectorizedRowBatch batch = reader.getSchema().createRowBatch(20);
-        batch.projectionSize = contCol;
+//        Reader.Options readOptions = reader.options();
+//        readOptions.include(finalProjection);
+//        readOptions.setRowFilter(new String[]{"val"}, consumer);
+
+//        SearchArgument mm = SearchArgumentFactory.newBuilder()
+//                .equals("name", PredicateLeaf.Type.LONG, 17L)
+//                .build();
+//
+//        Reader.Options readOptions = new Reader.Options(conf);
+//        readOptions.allowSARGToFilter(true);
+//        readOptions.searchArgument(mm, new String[]{"val"});
+
+
+        Reader.Options readOptions = reader.options();
+        readOptions.setRowFilter(new String[]{"val"}, consumer);
+
+
+
+
+        RecordReaderImpl records = (RecordReaderImpl) reader.rows(readOptions);
+
+        VectorizedRowBatch batch = reader.getSchema().createRowBatchV2();
+        int b = 0;
+        int t = 0;
         while (records.nextBatch(batch)) {
-            System.out.println(batch.toString());
+            TypeDescription td = new TypeDescription(TypeDescription.Category.STRUCT);
+            VectorizedRowBatch vv = new VectorizedRowBatch(20);
+            System.out.printf("" + batch.selectedInUse);
+            ;
+            for (int i = 0; i < batch.cols.length; i++) {
+                if (batch.projectedColumns[i] == 1) {
+                    td.addField(names.get(i), new TypeDescription(TypeDescription.Category.LONG));
+                    vv.cols[b] = batch.cols[i];
+                    b++;
+                }
+            }
+            vv.setFilterContext(true, batch.getSelected(), (int) batch.count());
+
+            OrcFile.WriterOptions options = OrcFile.writerOptions(conf).overwrite(true).setSchema(td);
+            Path pathO = new Path("/JavaCode/results" + t);
+            t++;
+            Writer writer = OrcFile.createWriter(pathO, options);
+            writer.addRowBatch(vv);
+            writer.close();
         }
         records.close();
+        batch.reset();
     }
 
     public static void writer(VectorizedRowBatch a, String path, String schemaStruct, String values) throws IOException, URISyntaxException, ParseException {
--- testORC/src/main/java/orc/main.java
+++ testORC/src/main/java/orc/main.java
@@ -2,27 +2,41 @@ package orc;
 
 
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
+import org.apache.orc.OrcFilterContext;
+import org.apache.orc.impl.OrcFilterContextImpl;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.function.Consumer;
 
 public class main {
-    //insert /JavaCode/testORC struct<name:string,val:int> {\"values\":[[\"13\",\"4\"],[\"23\",\"3\"],[\"33\",\"10\"]]}
+    //insert /JavaCode/testORC struct<name:string,val:int> {\"values\":[[\"Kristal\",\"3\"],[\"al\",\"4\"],[\"bob\",\"17\"],[\"Bi\",\"34\"],[\"col\",\"6\"],[\"Jil\",\"4\"],[\"sam\",\"3\"],[\"Dead\",\"0\"]]}
     //read /JavaCode/testORC name {"conditions": [["name", "0", "13"], ["val", "-1", "3"]]}
     static public void main(String[] arg) throws IOException, URISyntaxException, ParseException {
 
 
-        VectorizedRowBatch row = new VectorizedRowBatch(2, 10);
-        if(arg[0].equals("insert"))
+//        VectorizedRowBatch row = new VectorizedRowBatch(2, 10);
+//        if(arg[0].equals("insert"))
+//
+//            ORC.writer(row, arg[1], arg[2], arg[3]);
+//        else{
+//            for (int i = 0; i<arg.length; i++){
+//                System.out.println(arg[i]);
+//            }
+//            ORC.reader(arg[1], arg[2]);
+//            ORC.readerPrint("/JavaCode/results0");
+//        }
 
-            ORC.writer(row, arg[1], arg[2], arg[3]);
-        else{
-            for (int i = 0; i<arg.length; i++){
-                System.out.println(arg[i]);
-            }
-            ORC.reader(arg[1], arg[2]);
-        }
+
+        ORCProjection mmm = new ORCProjection();
+        Consumer<OrcFilterContext> ll = mmm.builder("mmmm");
+
+
+
+
     }
 
 
