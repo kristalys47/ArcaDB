46aabeeea53ad294687dc3385eb00a5517e3a137
Abigail <kristalys.cba4@yahoo.com>
2023-02-23 03:50:46 -0400

    Ready para clasificar...

--- PythonWorkers/Worker.py
+++ PythonWorkers/Worker.py
@@ -3,9 +3,10 @@ import redis
 import json
 import alluxio
 import numpy as np
+import os
 from keras.models import Sequential, Model, load_model
 from alluxio import option
-r = redis.Redis(host='136.145.77.83', port=6379)
+r = redis.Redis(host='localhost', port=6379)
 client = alluxio.Client('136.145.77.107', 39999)
 
 def imgClas(directory, property, boolean):
@@ -40,6 +41,57 @@ def testModel():
     result = model.predict(im)
     print(result)
 
-# start()
+def createMetadata():
+    meta = {}
+    meta["files"] = os.listdir("archive/img_align_celeba/img_align_celeba/")
+    json_data = json.dumps(meta)
 
-testModel()
+    with open('metadata.json', 'w') as f:
+        f.write(json_data)
+
+
+def startLocal():
+    meta_file = "metafile.json"
+    encoding = "utf-8"
+    task = r.blpop("python", 0)[1].decode(encoding)
+    # type, file_location, function_name, property, boolean
+    print(task)
+    # json_dic = json.loads(task)
+    #
+    # print(json_dic)
+    #
+    # if json_dic["plan"]:
+    #     array = json_dic["plan"]
+    #     for n in array:
+    #         print(n)
+    #
+    # list = client.ls("/image")
+    json_meta = {}
+
+    with open("metadata.json", "r") as f:
+         json_meta = json.load(f)
+
+    json_results = {}
+    list = []
+
+    model = load_model('saved_model/model')
+    model.load_weights('saved_model/weights')
+
+
+
+    for n in json_meta["files"]:
+        print(n)
+        im = cv2.imread("archive/img_align_celeba/img_align_celeba/" + n)
+        im = cv2.resize(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), (178, 218)).astype(np.float32) / 255.0
+        im = np.expand_dims(im, axis=0)
+        result = model.predict(im)
+        prediction = np.argmax(result)
+        if prediction == 0:
+            list.append(n)
+
+    json_results["result"] = list
+    with open("results.json") as f:
+        f.write(json_results)
+
+startLocal()
+# testModel()
--- script/add-images.py
+++ script/add-images.py
@@ -6,4 +6,4 @@ r = redis.Redis(host='136.145.77.83', port=6379)
 r.flushall()
 
 tables = ["images"]
-r.rpush("images", "/images/")
+r.rpush("images", "/images")
